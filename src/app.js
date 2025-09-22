3eb661ca7ca7f58cec7ef91852e2857ffb74d538
Atharva Patil <patharva52@gmail.com>
2025-09-22 18:27:16 +0530

    implemented nested routes

--- src/app.js
+++ src/app.js
@@ -2,16 +2,33 @@ const express = require("express");
 
 const app = express();
 
-app.use("/home", (req, res) => {
-  res.send("From Home");
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log(req.query);
+    // next();
+    res.send({
+      firstName: "Atharva",
+      lastName: "Patil",
+    });
+  },
+  (req, res) => {
+    res.send("2 Route Handler");
+  }
+);
+
+app.post("/user/aa", (req, res) => {
+  console.log("POST API called");
+  res.send("POST API");
 });
 
-app.use("/dashboard", (req, res) => {
-  res.send("From Dashbaord");
+app.put("/user", (req, res) => {
+  res.send("PUT API");
+  console.log("POST APII called Succesfully");
 });
 
-app.use("/", (req, res) => {
-  res.send("hello");
+app.delete("/user", (req, res) => {
+  console.log("DELETE API CALLED");
 });
 
-app.listen(4000);
+app.listen(5000);
