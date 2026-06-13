7c6bf15a5372482597f956422b24b87ecae24d3c
Atharva Patil <ap31@Atharvas-MacBook-Air.local>
2026-06-13 11:27:32 +0530

    created delete api

--- src/app.js
+++ src/app.js
@@ -3,42 +3,73 @@ const app = express();
 const connectDb = require("./config/database");
 const User = require("./models/users");
 
+// middleware for handling json format to js object
 app.use(express.json());
 
-// get user by email
-app.get("/user", async (req, res) => {
-  const userEmail = req.body.emailId;
 
-  const user = await User.find({ emailId: userEmail });
+
+
+// delete user
+
+app.delete('/user', async (req,res)=>{
+
+  const userId = req.body._id
+  
   try {
-    if (user.length === 0) {
-      res.status(404).send("invalid request");
-    } else {
-      res.send(user);
+    const user = await User.findByIdAndDelete(userId)
+    await res.send('User deleted succefully')    
+
+    
+  } catch (error) {
+    res.status(400).send('User cannot be deleted')
+  }
+})
+
+// get user by email
+
+app.get('/user', async(req,res) => {
+  const userEmailId  = req.body.emailId;
+
+  const user = await User.find({emailId : userEmailId})
+
+  try {
+    if(user.length === 0 ){
+      res.status(404).send('Invalid Request')
+    }else{
+      await res.send(user)
+
     }
   } catch (error) {
-    res.status(400).send("Email not found");
+    res.status(400).send('Email not found')
   }
-});
+
+})
 
 // get all users for the feed
 
-app.get("/feed", async (req, res) => {
-  const allUsers = await User.find({});
+app.get('/feed',async(req,res)=>{
+  
+  try {
+    const users = await User.find({}) // passing empty object gives me all the user data from the database
+    await res.send(users)
+  } catch (error) {
+    res.status(400).send('user not found')
+  }
+})
 
-  res.send(allUsers);
-});
+app.post('/signup',async(req,res) => {
+console.log(req.body);
+  
 
-app.post("/signup", async (req, res) => {
-  const user = new User(req.body);
+  const user = new User(req.body)
 
   try {
-    await user.save();
-    res.send("user signed up successfully");
+    await user.save()
+    res.send('User signedup successfully')
   } catch (error) {
-    res.status(400).send("Error signing up user : " + error.message);
+    res.status(400).send("Error signing up user : " + error.message)
   }
-});
+})
 
 //first connect to the database and then start or listen to the server
 connectDb()
